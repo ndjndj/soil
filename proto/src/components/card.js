7d90481507cf6ed534a200c6fb8df797b9a88776
ndjndj <teq.ndj@gmail.com>
2021-11-14 23:57:20 +0900

    change margin with root

--- proto/src/components/card.js
+++ proto/src/components/card.js
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    margin:'2% 2%'
   },
   bullet: {
     display: 'inline-block',
