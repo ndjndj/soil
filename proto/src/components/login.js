93ee1440bfcbc952de7f3f3f40102e38ff2a2d26
ndjndj <teq.ndj@gmail.com>
2021-11-16 00:02:02 +0900

    bug fix

--- proto/src/components/login.js
+++ proto/src/components/login.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import TextField from '@material-ui/core';
-import FormControlLabel from '@material-ui/core';
-import Box from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Box from '@material-ui/core/Box';
 
 
 export default function Login() {
     return (
+        <Box component="form" noValidate>
+            <TextField
+                required
+                fullWidth
 
-        <TextField
+            />
+        </Box>
 
-        />
-    )
+    );
 }
