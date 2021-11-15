ce3cd56969349d405516a2ef3a55dbf9835f731d
ndjndj <teq.ndj@gmail.com>
2021-11-15 23:58:16 +0900

    login.js

---
+++ proto/src/components/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TextField from '@material-ui/core';
+import FormControlLabel from '@material-ui/core';
+import Box from '@material-ui/core';
+
+
+export default function Login() {
+    return (
+
+        <TextField
+
+        />
+    )
+}
