68388f2f578553f3df6de187e06d06d10cfb3854
ndjndj <teq.ndj@gmail.com>
2021-11-16 23:30:14 +0900

    import Header

--- proto/src/pages/create-issue.js
+++ proto/src/pages/create-issue.js
@@ -3,5 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Header from '../components/header';
 
 
