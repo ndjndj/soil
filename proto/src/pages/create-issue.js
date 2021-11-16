25359c2404ad7446895157d9bbb8e852927f0fca
ndjndj <teq.ndj@gmail.com>
2021-11-16 23:38:45 +0900

    import TextField

--- proto/src/pages/create-issue.js
+++ proto/src/pages/create-issue.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import Header from '../components/header';
 
 export default function CreateIssue() {
@@ -10,7 +11,12 @@ export default function CreateIssue() {
     return(
         <React.Fragment>
             <Header />
-        
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+
+                </Grid>
+            </Grid>
+
         </React.Fragment>
     );
 
