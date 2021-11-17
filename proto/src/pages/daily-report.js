983ebece952baca11669485d2ce6172540400629
ndjndj <teq.ndj@gmail.com>
2021-11-17 22:15:11 +0900

    skelton with daily-report.js

---
+++ proto/src/pages/daily-report.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Header from '../components/header';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles({
+
+});
+
+
+export default function CreateIssue() {
+    const classes = useStyles();
+    return(
+        <React.Fragment>
+            <Header />
+            
+        </React.Fragment>
+    );
+}
