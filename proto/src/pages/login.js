2fb25a0a14fcb776323c60ea93e8e59300a0feb7
ndjndj <teq.ndj@gmail.com>
2021-11-19 00:41:01 +0900

    fix

--- proto/src/pages/login.js
+++ proto/src/pages/login.js
@@ -1,45 +1,71 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Box from '@material-ui/core/Box';
+import Header from '../components/header';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container';
+
+
+const useStyles = makeStyles({
+    root: {
+        margin: '8% 10%'
+    },
+    container: {
+        margin: 'auto',
+    },
+    center: {
+        textAlign: 'center',
+    },
+    row: {
+        margin: '0 12% 0 12%'
+    }
+});
+
 
 export default function Login() {
-    return (
-        <Box component="form" noValidate>
-
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-            />
-
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="password"
-                label="Password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-            />
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{mt: 3, mb: 2}}
-            >
-                Sign In
-            </Button>
-
-        </Box>
-
-    );
+    const classes = useStyles();
+    return(
+        <React.Fragment>
+            <Header />
+            <Container maxWidth='lg'>
+                <Paper className={classes.root}>
+                    <form>
+                        <Grid
+                            container
+                            spacing={3}
+                            alignItems='center'
+                            justify='center'
+                        >
+                            <Grid item  xs={12}>
+                                <Typography
+                                    variant='h3'
+                                    component='h1'
+                                    color='textPrimary'
+                                    className={classes.center}
+                                >
+                                    Create Issue
+                                </ Typography>
+                            </Grid>
+                            <Grid item  xs={12} className={classes.row}>
+                                <TextField variant='outlined' label='code' fullWidth />
+                            </Grid>
+                            <Grid item  xs={12} className={classes.row}>
+                                <TextField variant='outlined' label='name' fullWidth />
+                            </Grid>
+                            <Grid item xs={12}  className={classes.row}>
+                                <TextField variant='outlined' label='issue' fullWidth/>
+                            </Grid>
+                            <Grid item xs={12}  className={classes.center}>
+                                <Button color='primary' variant='contained'>
+                                    Create
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Paper>
+            </Container>
+        </React.Fragment>
 }
