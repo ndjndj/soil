11d7f804bde94ceb7d4a1c1637c137676d5a4c5c
ndjndj <teq.ndj@gmail.com>
2021-11-18 23:15:17 +0900

    implement TabPanel

--- proto/src/pages/daily-report.js
+++ proto/src/pages/daily-report.js
@@ -11,11 +11,31 @@ import Tabs from '@material-ui/core/Tabs';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
 
 });
 
+function TabPanel(props) {
+    const {children, value, index, ...other} = props;
+
+    return (
+        <div
+            role='tabpanel'
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{p: 3}}>
+                        <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
 
 export default function DailyReport() {
 
