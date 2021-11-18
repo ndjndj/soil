15af4cad0ea61b3d20c40ebad9984c19c0c741f2
ndjndj <teq.ndj@gmail.com>
2021-11-19 00:39:14 +0900

    change dir with login.js

--- proto/src/App.js
+++ proto/src/App.js
@@ -4,14 +4,14 @@ import SimpleList from "./components/listsample";
 import SimpleCard from "./components/card";
 import { Container } from "@material-ui/core";
 import ListItem from "./components/listItem";
-import Login from "./components/login";
+import Login from "./pages/login";
 import CreateIssue from "./pages/create-issue";
 import DailyReport from "./pages/daily-report";
 
 function App() {
   return (
     <div className="App">
-      <DailyReport />
+      <Login/>
     </div>
   );
 }
