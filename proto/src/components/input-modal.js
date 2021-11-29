bc94b57c4044fe11dd404975ed504c8e3ebd5b68
ndjndj <teq.ndj@gmail.com>
2021-11-29 23:33:24 +0900

    input-modal,js

---
+++ proto/src/components/input-modal.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Header from '../components/header';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+export default function InputModal() {
+    return(
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
