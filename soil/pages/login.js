ac58abb0695255da822d75216645481747623464
ndjndj <teq.ndj@gmail.com>
2021-12-10 00:48:31 +0900

    fix

--- soil/pages/login.js
+++ soil/pages/login.js
@@ -4,9 +4,9 @@ import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Header from '../components/header';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Header from '../src/components/header';
 import axios from 'axios';
 
 const useStyles = makeStyles({
