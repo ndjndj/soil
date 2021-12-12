c3db53db60090637c0d3cd8d9b81c9f1e123c938
ndjndj <teq.ndj@gmail.com>
2021-12-12 23:30:06 +0900

    issue-list

--- soil/pages/issue-list.js
+++ soil/pages/issue-list.js
@@ -9,12 +9,80 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import axios from 'axios';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minWidth: 275,
+        margin:'2% 2%'
+    },
+    title: {
+        fontSize: 18,
+        paddingTop: '5%',
+        paddingLeft: '1%',
+    }
+}));
+
 export default function IssueList() {
 
-
+    const classes = useStyles();
     return (
         <React.Fragment>
             <Header />
+            <Paper className={classes.root}>
+        <Grid container spacing={3}>
+            <Grid item xs>
+                <Typography variant="h2" className={classes.title}>title</Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography component="p">created day: </Typography>
+                <Typography component="p">updated day: </Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography variant="h5">type </Typography>
+            </Grid>
+        </Grid>
+    </Paper>
+    <Paper className={classes.root}>
+        <Grid container spacing={3}>
+            <Grid item xs>
+                <Typography variant="h2" className={classes.title}>title</Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography component="p">created day: </Typography>
+                <Typography component="p">updated day: </Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography variant="h5">type </Typography>
+            </Grid>
+        </Grid>
+    </Paper>
+    <Paper className={classes.root}>
+        <Grid container spacing={3}>
+            <Grid item xs>
+                <Typography variant="h2" className={classes.title}>title</Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography component="p">created day: </Typography>
+                <Typography component="p">updated day: </Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography variant="h5">type </Typography>
+            </Grid>
+        </Grid>
+    </Paper>
+    <Paper className={classes.root}>
+        <Grid container spacing={3}>
+            <Grid item xs>
+                <Typography variant="h2" className={classes.title}>title</Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography component="p">created day: </Typography>
+                <Typography component="p">updated day: </Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography variant="h5">type </Typography>
+            </Grid>
+        </Grid>
+    </Paper>
         </React.Fragment>
     );
 }
