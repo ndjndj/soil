da398c153f28bececcb0563b914bd3ae425711df
ndjndj <teq.ndj@gmail.com>
2021-12-12 23:23:02 +0900

    fix

--- soil/pages/create-user.js
+++ soil/pages/create-user.js
@@ -4,7 +4,7 @@ import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Header from '../components/header';
+import Header from '../src/components/header';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import axios from 'axios';
