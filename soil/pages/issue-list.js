2ba5cdac2deb88c01296ef0ead1c2b95dea9536b
ndjndj <teq.ndj@gmail.com>
2021-12-14 00:35:54 +0900

    fix

--- soil/pages/issue-list.js
+++ soil/pages/issue-list.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Header from '../src/components/header';
-import Typography from '@material-ui/core/Typography';
 import ListItem from '../src/components/lsit-item';
-import Container from '@material-ui/core/Container';
 import axios from 'axios';
 
 
