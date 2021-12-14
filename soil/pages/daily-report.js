40d945ee404625afda66973b5c020be7062cfbea
ndjndj <teq.ndj@gmail.com>
2021-12-14 23:59:47 +0900

    fix

--- soil/pages/daily-report.js
+++ soil/pages/daily-report.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Header from '../components/header';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Tabs from '@material-ui/core/Tabs';
 import SimpleMDE from 'react-simplemde-editor';
